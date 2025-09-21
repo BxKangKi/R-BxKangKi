9fff171f8df50ea8a429b5c0293c21ab256975dd
Kiyeon Kang <80934730+BxKangKi@users.noreply.github.com>
2025-09-21 16:10:47 +0900

    Initial commit

---
+++ R/bxkangki.R
@@ -0,0 +1,7 @@
+#' @name bxkangki.info
+#' @title bxkangki.info
+#' @export
+bxkangki.info = function() {
+  print("[ BxKangKi R Package ] ")
+  print("This is personal R package")
+}
---
+++ R/math.R
@@ -0,0 +1,8 @@
+#' @name math.slope
+#' @title math.slope
+#' @export
+math.slope = function(B, A, b, a) {
+  return((B - A) / (b - a))
+}
+
+
